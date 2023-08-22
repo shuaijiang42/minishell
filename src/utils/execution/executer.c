2c9308f4e97feb184e7e40e44c93be08f4c1a4d7
samusanc <samusanc@student.42madrid.com>
2023-08-22 16:54:34 +0200

    commit before branch

--- src/utils/execution/executer.c
+++ src/utils/execution/executer.c
@@ -6,7 +6,7 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 15:50:18 by samusanc          #+#    #+#             */
-/*   Updated: 2023/08/22 16:04:40 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/08/22 16:53:40 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,40 @@
 
 void	ft_exc_in_redir()
 {
-
+	/*
+	while (list->content == 'inp')
+	
+	while (list->content == 'here doc')
+	*/
+	return ;
 }
 
 void	ft_exc_out_redir()
 {
+	/*
+	while (list->content == 'inp')
 
+	while (list->content == 'here doc')
+	*/
+	return ;
 }
 
 char	*ft_exc_make_cmd()
 {
-
+	//make the entire cmd with arguments
+	//	new_cmd = ft_get_cmd();
 }
 
-int		ft_exc_execution()
+int		ft_exc_execution(char *cmd, char **env)
 {
 	input = ft_lexer(new_cmd);
 	if (!input[0][0])
 	{
 		ft_print_error(": command not found", 127);
-		value = 127;
+		return (127);
 	}
 	else
-		value = ft_excuter(input, env);
-}
-
-void ft_exc_make_redir()
-{
-	//  ft_exc_in_redir
-	{
-		while (list->content == 'inp')
-		
-		while (list->content == 'here doc')
-
-	}
-	//  ft_exc_out_redir
-	{
-
-		while (list->content == 'inp')
-
-		while (list->content == 'here doc')
-
-	}
-	//	ft_exc_make_cmd
-	{
-		//make the entire cmd with arguments
-	//	new_cmd = ft_get_cmd();
-	}
-	//	ft_exc_execution
+		return (ft_excuter(input, env));
 }
 
 t_list	ft_exc_lex_input(char *cmd)
@@ -69,26 +55,39 @@ t_list	ft_exc_lex_input(char *cmd)
 
 }
 
+int ft_exc_make_redir(char *cmd, char **env)
+{
+	int		result;
+	t_list	*input;
+	char	*new_cmd;
+
+	input = ft_exc_lex_input(cmd);
+	//  ft_exc_in_redir
+	ft_exc_in_redir();
+	//  ft_exc_out_redir
+	ft_exc_out_redir();
+	//	ft_exc_make_cmd
+	new_cmd = ft_exc_make_cmd();
+	//	ft_exc_execution
+	result = ft_exc_execution(new_cmd, env);
+	return (result);
+}
+
+
 int	executer(char *cmd, char **env)
 {
 	//this executer need to be improved, it does not count with the redirections
-	char **input;
-	char	*new_cmd;
+	int		cloud[2];
+	int		value;
 
 
-	//ft_exc_lex_input
-	{
-		//lex the list
-		//lex cmd list;
-	}
+	
 	//this code belong to other place, i think the execute cmd, so maybe u can try to move it there?
 	//-----------------------------------------------------------//
-	int	cloud[2];
-	int	value;
 	
 	cloud[0] = dup(0);
 	cloud[1] = dup(1);
-	ft_exc_make_redir();
+	value = ft_exc_make_redir();
 	dup2_with_error_check(cloud[1], 1);
 	close(cloud[1]);
 	dup2_with_error_check(cloud[0], 0);
