e6af82cad05b4f1563c3c913a8b8deb75e47b6bd
samusanc <samusanc@student.42madrid.com>
2023-09-24 18:00:21 +0200

    uploading the procces executer norminette

--- src/utils/execution/proccess/proccess.c
+++ src/utils/execution/proccess/proccess.c
@@ -6,47 +6,57 @@
 /*   By: shujiang <shujiang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 15:16:47 by samusanc          #+#    #+#             */
-/*   Updated: 2023/09/22 18:50:35 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/09/24 18:00:01 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+
+static void proccess_pipex(char *cmd)
+{
+	int pid;
+	int	status;
+
+	flag = PROCCESS;
+	pid = fork_with_error_check();
+	ft_put_proccess(1);
+	if (!pid)
+	{
+		flag = 4;
+		pipex(cmd);
+	}
+	waitpid(-1, &status, 0);
+	ft_put_error(WEXITSTATUS(status));
+}
+
+static void	proccess_solo(char *cmd)
+{
+	int pid;
+	t_input	line;
+
+	pid = ft_exc_make_redir(cmd, &line);
+	if (pid)
+	{
+		ft_put_error(pid);
+		return ;
+	}
+	ft_is_valid_in(STDIN_FILENO, &line);
+	ft_put_error(executer(cmd, &line));
+}
+
 void	ft_procces_maker(char *cmd, char **env)
 {
-	char **input;
-	int		pid;
-	int		status;
-	t_input	line;
+	char	**input;
 
 	input = ft_lexer(cmd);
 	if (input)
 	{
 		ft_free_split_2(&input);
 		if (count_pipes(cmd) > 0)
-		{
-			flag = PROCCESS;
-			pid = fork_with_error_check();
-			ft_put_proccess(1);
-			if (!pid)
-			{
-				flag = 4;
-				pipex(cmd);
-			}
-			waitpid(-1, &status, 0);
-			ft_put_error(WEXITSTATUS(status));
-		}
+			proccess_pipex(cmd);
 		else
-		{
-			pid = ft_exc_make_redir(cmd, &line);
-			if (pid)
-			{
-				ft_put_error(pid);
-				return ;
-			}
-			ft_is_valid_in(STDIN_FILENO, &line);
-			ft_put_error(executer(cmd, &line));
-		}
+			proccess_solo(cmd);
 	}
 	else
 		ft_free_split_2(&input);
